b2d524fbd1e2f14c54982eb5de878100f5bb5b8e
not-meet <mjmeet.279@gmail.com>
2024-12-01 18:07:25 +0530

    initialised liveblocks and auth

--- main/app/(root)/documents/[id]/page.tsx
+++ main/app/(root)/documents/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { Editor } from "@/components/editor/Editor";
 import Header from "@/components/Header";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const Document = () => {
@@ -7,7 +8,14 @@ const Document = () => {
     <div>
       <Header className="flex w-fit items-center justify-center gap-2">
         <p className="document-title">this is a fake doccument title</p>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </Header>
+
       <Editor />
     </div>
   )
