997c9c2680041d0fcdfeef0b3f382ca850df0c9b
not-meet <mjmeet.279@gmail.com>
2024-12-03 22:16:04 +0530

    added the title changer

--- main/lib/actions/room.actions.ts
+++ main/lib/actions/room.actions.ts
@@ -1,5 +1,4 @@
 'use server';
-
 import { nanoid } from 'nanoid'
 import { liveblocks } from '../liveblocks';
 import { revalidatePath } from 'next/cache';
@@ -30,5 +29,32 @@ export const createDocument = async ({ userId, email }: CreateDocumentParams) =>
     return parseStringify(room);
   } catch (error) {
     console.log(`Error happened while creating a room: ${error}`);
+  }
+}
+
+export const getDocument = async ({ roomId, userId }: { roomId: string, userId: string }) => {
+  try {
+    const room = await liveblocks.getRoom(roomId);
+    // const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+    // if (!hasAccess) throw new Error('You do not have acces to this document');
+    //
+    return parseStringify(room);
+  } catch (error) {
+    console.log(`something went wrong : ${error}`);
+  }
+}
+
+
+export const updateDocument = async (roomId: string, title: string) => {
+  try {
+    const updateRoom = await liveblocks.updateRoom(roomId, {
+      metadata: {
+        title
+      }
+    })
+    revalidatePath(`/document/${roomId}`);
+    return parseStringify(updateRoom);
+  } catch (error) {
+    console.log(`something went wrong while updating ${error}`);
   }
 }
