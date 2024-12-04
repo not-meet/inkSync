f5ad6e1cdc1163c75fffc41fa3197ad9c003102b
not-meet <mjmeet.279@gmail.com>
2024-12-04 22:20:57 +0530

    added the email sharing modal

--- main/components/CollaborativeRoom.tsx
+++ main/components/CollaborativeRoom.tsx
@@ -12,6 +12,7 @@ import { Input } from "./ui/input";
 import Image from "next/image";
 import { updateDocument } from "@/lib/actions/room.actions";
 import Loader from "./Loader";
+import ShareModal from "./ShareModal";
 const CollaborativeRoom = ({ roomId, roomMetadata, users, currentUserType }: CollaborativeRoomProps) => {
   const [documentTitle, setDocumentTitle] = useState(roomMetadata.title);
   const [editing, setEditing] = useState(false);
@@ -102,6 +103,7 @@ const CollaborativeRoom = ({ roomId, roomMetadata, users, currentUserType }: Col
               <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
                 <ActiveCollaborators />
 
+                <ShareModal roomId={roomId} collaborators={users} creatorId={roomMetadata.creatorId} currentUserType={currentUserType} />
 
                 <SignedOut>
                   <SignInButton />
